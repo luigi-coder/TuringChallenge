4d85525ec8c4dae3d52bef22b322d175fab49623
luigi-coder <luigicoder28@gmail.com>
2022-11-01 01:17:14 -0300

    Pruebas Unitarias

--- js/test.spec.js
+++ js/test.spec.js
@@ -1,29 +1,63 @@
-// realizar test unitarios para la respuesta del fecth
-function revisar_sintaxis_titulo(){
+function run_test(){
 
-    const data = document.getElementById("titulo").innerHTML
-    const regex = /^[a-zA-Z0-9 ]{3,30}$/;
-    const result = regex.test(data);
-    console.log(result);
-    if (typeof data === 'string') console.log("El titulo es un string");
-
-    if(data == "Consulta de saldo") console.log("Titulo correcto");
+    revisar_titulos_cards_cuentas()
+    revisar_numeros_cuentas()
     
+    if(revisar_paginacion()){
+        console.error("ERROR: El test no permite leer mas paginas de las posibles");
+    }else {
+        console.info("El test permite leer mas paginas de las posibles");
+    }
 }
 
-//revisar_sintaxis_titulo();
 
-function revisar_titulos_cards_cuentas(){
+// FUNCION ENCARGADA DE REVISAR LOS TITULOS DE CADA TARJETA DE CADA CUENTA
+function revisar_titulos_cards_cuentas() {
 
-    const titulo = document.querySelectorAll("#tituloTipoCuenta");
-    console.log(titulo);
-    
-    for (let i = 0; i < titulo.length; i++) {
-        const element = titulo[i].innerHTML;
-        console.log(element);
-    } 
+    const titulo = document.querySelectorAll(".tituloTipoCuenta");
 
+    console.log("TESTEANDO LOS TITULOS DE LAS CARDS CUENTAS");
 
+    titulo.forEach(element => {
+        let cuentaTipo = element.innerHTML;
+        if (cuentaTipo === "Caja de Ahorro") console.log("Caja de Ahorro - Titulo correcto");
+        if (cuentaTipo === "Cuenta Corriente") console.log("Cuenta Corriente - Titulo correcto");
+    });
 }
 
-revisar_titulos_cards_cuentas();
+// FUNCION ENCARGADA DE REVISAR QUE LOS NUMEROS DE LAS CUENTAS TENGAS 12 DIGITOS
+function revisar_numeros_cuentas() {
+
+    const nroCuentas = document.querySelectorAll(".nroCuentas");
+    console.log("TESTEANDO LOS NUMEROS DE LAS CUENTAS");
+
+    nroCuentas.forEach((element) => {
+
+        let cuentaNro = element.innerHTML;
+        cuentaNro = cuentaNro.substring(4);
+
+        if (cuentaNro.length === 12) {
+            console.log("Numero de cuenta correcto")
+        } else {
+            console.log("Se encontro una cuenta sin numero de cuenta");
+        }
+    })
+}
+
+// FUNCION ENCARGADA DE NO PERMITIR LEER MAS PAGINAS DE LAS POSIBLES
+function revisar_paginacion() {
+
+    const btnSiguiente = document.getElementById("h3B");
+
+    try{
+        for(let i = 0; i < 100; i++){
+            btnSiguiente.click();
+            console.log("Click" + (i+1));
+        }
+        return true;
+    }
+    catch(err){
+        console.log("No se puede seguir paginando");
+        return false;
+    }
+}
